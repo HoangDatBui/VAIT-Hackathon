739f3840157302d69f97e6e854912849e4c5ba1f
Minh Luu <PyGeek03@users.noreply.github.com>
2025-03-29 15:07:24 +1100

    Add script for bot

---
+++ main.py
@@ -0,0 +1,35 @@
+# Source: https://discordpy.readthedocs.io/en/stable/quickstart.html
+
+# How to run:
+# 1) Add DISCORD_TOKEN environment variable to store bot's token
+# 2) Run `python main.py` to start the bot
+# 3) Send `$hello` to see the bot respond
+
+import os
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello!")
+
+
+token = os.getenv("DISCORD_TOKEN")
+if token is None:
+    raise ValueError("DISCORD_TOKEN is not set")
+
+client.run(token)
