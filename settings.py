494bed69ebbb212b0c5e85de18abf05462e9fe31
Minh Luu <PyGeek03@users.noreply.github.com>
2025-03-30 01:08:14 +1100

    chore: Remove unused variables & imports

--- settings.py
+++ settings.py
@@ -1,8 +1,5 @@
 import os
-import logging
-import discord
 from dotenv import load_dotenv
-from logging.config import dictConfig
 
 load_dotenv()
 
@@ -13,41 +10,3 @@ if DISCORD_TOKEN is None:
 DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
 if DEEPSEEK_API_KEY is None:
     raise ValueError("DEEPSEEK_API_KEY is not set")
-
-LOGGING_CONFIG = {
-    "version": 1,
-    "disabled_existing_loggers": False,
-    "formatters": {
-        "verbose": {
-            "format": "%(levelname)-10s - %(asctime)s - %(module)-15s : %(message)s"
-        },
-        "standard": {"format": "%(levelname)-10s - %(name)-15s : %(message)s"},
-    },
-    "handlers": {
-        "console": {
-            "level": "DEBUG",
-            "class": "logging.StreamHandler",
-            "formatter": "standard",
-        },
-        "console2": {
-            "level": "WARNING",
-            "class": "logging.StreamHandler",
-            "formatter": "standard",
-        },
-        "file": {
-            "level": "INFO",
-            "class": "logging.FileHandler",
-            "filename": "logs/infos.log",
-            "mode": "w",
-            "formatter": "verbose",
-        },
-    },
-    "loggers": {
-        "bot": {"handlers": ["console"], "level": "INFO", "propagate": False},
-        "discord": {
-            "handlers": ["console2", "file"],
-            "level": "INFO",
-            "propagate": False,
-        },
-    },
-}
